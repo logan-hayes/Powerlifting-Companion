7ad475a9d585371c85146e50d2787d6f0794612a
logan <logan.hayes111@gmail.com>
2026-03-25 01:21:27 -0400

    Completed Home screen, additional card elements added

--- app/src/main/java/com/example/powerlifter_companion/ui/HomeScreen.kt
+++ app/src/main/java/com/example/powerlifter_companion/ui/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.powerlifter_companion.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
@@ -13,19 +14,234 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.background
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Brush
 import com.example.powerlifter_companion.ui.theme.BackgroundGray
-
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.List
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import com.example.powerlifter_companion.ui.theme.PrimaryRed
-
+import com.example.powerlifter_companion.R
+import androidx.compose.ui.text.font.FontFamily
 
 @Composable
-fun HomeScreen(){
+fun TopGreeting() {
+    Column {
+        Text(
+            text = "Hello Logan",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+
+@Composable
+fun NextWorkoutCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(22.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.82f)
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
+        border = BorderStroke(
+            1.dp,
+            Color.White.copy(alpha = 0.08f)
+        )
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Box(
+                    modifier = Modifier
+                        .size(42.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.primary.copy(alpha = 0.15f),
+                        ), contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Next workout",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Text(
+                    text = "Start Next Workout",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Hypertrophy Week 1 day 4",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(14.dp)
+            )
+            {
+                Text("Start Workout")
+            }
+        }
+    }
+}
+
+@Composable
+fun WeeklyExposureCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(22.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.88f)
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        border = BorderStroke(
+            1.dp,
+            Color.White.copy(alpha = 0.08f)
+        )
+    ) {
+        Column(modifier = Modifier.padding(14.dp)) {
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Weekly Exposure",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+
+                Text(
+                    text = "Weekly Exposure",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            Spacer(modifier = Modifier.height(6.dp))
+
+            WeeklyExposureRow("Squat", "High")
+            Spacer(modifier = Modifier.height(4.dp))
+            WeeklyExposureRow("Bench", "High")
+            Spacer(modifier = Modifier.height(4.dp))
+            WeeklyExposureRow("Deadlift", "Low")
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+}
+
+@Composable
+fun WeeklyExposureRow(liftName: String, exposure: String){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = liftName,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Text(
+            text = exposure,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.SemiBold
+        )
+    }
+}
+
+@Composable
+fun CurrentBlockCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(22.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.88f)
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
+        border = BorderStroke(
+            1.dp,
+            Color.White.copy(alpha = 0.08f)
+        )
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.List,
+                    contentDescription = "Current Block",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "Current Block",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Branding(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Icon(
+                painter = painterResource(R.drawable.fitness_center_24dp_1f1f1f_fill0_wght400_grad0_opsz24),
+                contentDescription = "Barbell",
+                tint = MaterialTheme.colorScheme.primary.copy(alpha = .88f),
+                modifier = Modifier.size(80.dp)
+            )
+
+            Spacer(modifier.height(8.dp))
+
+            Text(
+                text = "Powerlifter's Companion",
+                style = MaterialTheme.typography.headlineLarge,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f)
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
     val gradientBlack = Brush.verticalGradient(
         colors = listOf(
             BackgroundGray,
@@ -33,15 +249,22 @@ fun HomeScreen(){
         )
     )
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(gradientBlack)
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.Top
-    ){
-        Text("Home screen UI test")
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradientBlack)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Top
+        ) {
+            TopGreeting()
+            Spacer(modifier = Modifier.height(24.dp))
+            NextWorkoutCard()
+            Spacer(modifier = Modifier.height(24.dp))
+            WeeklyExposureCard()
+            Spacer(modifier = Modifier.height(24.dp))
+            CurrentBlockCard()
+            Spacer(modifier = Modifier.height(40.dp))
+            Branding()
+        }
     }
-}
